19a0235d77e94a855e4cd090b2a37f0444cf5d69
Dad <Chrisvin@chrisvins-mbp.lan>
2018-08-11 10:59:55 -0400

    Convert to MVC design pattern.

---
+++ Data Model/New Group/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Chrisvin Jabamani on 8/10/18.
+//  Copyright © 2018 Chrisvin Jabamani. All rights reserved.
+//
+
+import Foundation
+
+
+class Item{
+    
+    let string
+}
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Chrisvin Jabamani on 8/10/18.
+//  Copyright © 2018 Chrisvin Jabamani. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title : String = ""
+    var done : Bool = false
+}
